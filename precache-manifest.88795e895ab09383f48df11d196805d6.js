5796ca1a825dc25a25f4c90579bb8e12545d8bf4
unknown <nguyentruongthanhtam@users.noreply.github.com>
2019-01-28 17:14:48 +0200

    Updates

---
+++ precache-manifest.88795e895ab09383f48df11d196805d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "40f00d6e0b3a05ec9b9ddfc5fc685c49",
+    "url": "/ReactWeather/static/media/find.40f00d6e.svg"
+  },
+  {
+    "revision": "fb0a94093a71ec8017daab3aa7f89925",
+    "url": "/ReactWeather/static/media/drop.fb0a9409.svg"
+  },
+  {
+    "revision": "a2622fa2ad826d6da5df",
+    "url": "/ReactWeather/static/js/runtime~main.a2622fa2.js"
+  },
+  {
+    "revision": "1a1f6a0428149417f7c7",
+    "url": "/ReactWeather/static/js/main.1a1f6a04.chunk.js"
+  },
+  {
+    "revision": "25f85146bada04357e36",
+    "url": "/ReactWeather/static/js/1.25f85146.chunk.js"
+  },
+  {
+    "revision": "1a1f6a0428149417f7c7",
+    "url": "/ReactWeather/static/css/main.1e5356f5.chunk.css"
+  },
+  {
+    "revision": "46d0d97b7f10469f3b990915d35fc10c",
+    "url": "/ReactWeather/index.html"
+  }
+];
